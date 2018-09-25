b0171848c0a9517696416b2d30b57bc5f76af2d7
herikudaru <pihlbuster@gmail.com>
2018-09-25 16:50:04 +0300

    Testpage

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>HTML testpage</title>
+	
+	<meta name="description" content="testpage">
+
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+	<h1>HTML testpage</h1>
+	
+	<p>Lorem ipsum dolor sit amet...</p>
+	
+</body>
+</html>
