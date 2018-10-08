a038275d97801be20a9a63f859084dd815de87e7
Buster <pihlbuster@gmail.com>
2018-10-08 12:27:54 +0300

    Finished frontpage base template

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 <body>
 
 	<div class="col-12" id="topbar">
-		<div class="col-9"><p></p></div>
-		<div class="col-3">
+		<div class="col-10"><h4>Shoe shop</h4></div>
+		<div class="col-2">
 			<button>Cart</button>
 			<button>Login</button>
 		</div>
@@ -32,19 +32,20 @@
 		<div class="dropdown">
 			<button class="dropbtn">Size</button>
 			<div class="dropdown-content">
-				<input type="radio" name="size" value="1">1
-				<input type="radio" name="size" value="2">2
-				<input type="radio" name="size" value="3">3
-				<input type="radio" name="size" value="4">4
-				<input type="radio" name="size" value="5">5
-				<input type="radio" name="size" value="6">6
+				<input type="radio" name="size" value="0">all</br>
+				<input type="radio" name="size" value="1">1</br>
+				<input type="radio" name="size" value="2">2</br>
+				<input type="radio" name="size" value="3">3</br>
+				<input type="radio" name="size" value="4">4</br>
+				<input type="radio" name="size" value="5">5</br>
+				<input type="radio" name="size" value="6">6</br>
 			</div>
 		</div>
 		<div class="dropdown">
 			<button class="dropbtn">Price</button>
 			<div class="dropdown-content">
-				<input type="text" name="price_beginning" value="0">
-				<input type="text" name="price_ending" value="1000">
+				<input type="text" name="price-beginning" value="0">
+				<input type="text" name="price-ending" value="1000">
 			</div>
 		</div>
 		<div class="dropdown">
@@ -66,6 +67,20 @@
 			</div>
 		</div>
 		<button>Filter</button>
+	</div>
+	
+	<div class="col-12" id="product-list">
+		<table>
+			<tr>
+				<th>Image</th>
+				<th>Name</th>
+				<th>Brand</th>
+				<th>Price</th>
+			</tr>
+			<?php
+				//populate the product list with php
+			?>
+		</table>
 	</div>
 	
 </body>
