3d2cce94fceb2db16e7d21f2ee3472cbcbeeb813
gronroma <maxgronroos@gmail.com>
2018-10-15 16:15:31 +0300

    initial loginpage commit

---
+++ login.php
@@ -0,0 +1,29 @@
+
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>HTML testpage</title>
+	
+	<meta name="description" content="testpage">
+
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+	<?php require 'topbar.php'; ?>
+	
+	<form>
+  Email:<br>
+  <input type="text" name="email"><br>
+  Password:<br>
+  <input type="text" name="password">
+  <br>
+  <button type="button">Login</button> <button type="button">Register</button> 
+</form>
+	
+</body>
+</html>
