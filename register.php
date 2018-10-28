5ccf1d278c16f82836e6be76f30648affc7dfa6b
gronroma <maxgronroos@gmail.com>
2018-10-28 03:41:09 +0300

    added register page

---
+++ register.php
@@ -0,0 +1,49 @@
+
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+
+	<title>HTML testpage</title>
+	
+	<meta name="description" content="testpage">
+
+	<link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+
+	<?php require 'topbar.php'; ?>
+	
+	<form>
+  Name:
+  <br>
+  <input type="text" name="name">
+  <br>
+  Email:
+  <br>
+  <input type="text" name="email">
+  <br>
+  Password:
+  <br>
+  <input type="text" name="password">
+  <br>
+  Homeadress:
+  <br>
+  <input type="text" name="homeadress">
+  <br>
+  Postnumber:
+  <br>
+  <input type="text" name="postnumber">
+  <br>
+  City:
+  <br>
+  <input type="text" name="city">
+  <br>
+  <button type="button">Register</button> 
+</form>
+	
+	
+</body>
+</html>
