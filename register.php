7a704bd942fea91f6ac1e9d7ded507e9b2d0926b
gronroma <maxrgronroos@gmail.com>
2018-11-04 18:08:40 +0200

    almost working register page

--- register.php
+++ register.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 
 <html lang="en">
@@ -9,41 +8,62 @@
 	
 	<meta name="description" content="testpage">
 
-	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="css/styles.css">    
+	
+	<link rel="shortcut icon" href="#" />
+	
 </head>
+
 
 <body>
 
 	<?php require 'topbar.php'; ?>
-	
-	<form>
-  Name:
+
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<form id="register_form" action="https://webshop-userdb-api.herokuapp.com/Users/register" method="post">
+  <input type="text" placeholder="Name" name="name" title="Up to 20 alphabetical characters">
   <br>
-  <input type="text" name="name">
+  <input type="email" placeholder="Email" name="email" title="Must be a valid email address">
   <br>
-  Email:
+  <input type="password" placeholder="Password" name="password" title="Must be 8 or more characters long and contain at least one number and one uppercase letter">
   <br>
-  <input type="text" name="email">
+  <input type="text" placeholder="Homeadress" name="homeadress">
   <br>
-  Password:
+  <input type="text" placeholder="Postnumber" name="postnumber">
   <br>
-  <input type="text" name="password">
+  <input type="text" placeholder="City" name="city">
   <br>
-  Homeadress:
-  <br>
-  <input type="text" name="homeadress">
-  <br>
-  Postnumber:
-  <br>
-  <input type="text" name="postnumber">
-  <br>
-  City:
-  <br>
-  <input type="text" name="city">
-  <br>
-  <button type="button">Register</button> 
+  <button value="Submit" type="submit">Register</button>
 </form>
-	
-	
+
+<script>
+
+const serialize_form = form => JSON.stringify(
+  Array.from(new FormData(form).entries())
+       .reduce((m, [ key, value ]) => Object.assign(m, { [key]: value }), {})
+);
+
+$('#register_form').on('submit', function(event) {
+  event.preventDefault();
+  const json = serialize_form(this);
+   console.log(json);
+ 
+ $.ajax({
+    type: 'POST',
+    url: 'https://webshop-userdb-api.herokuapp.com/Users/register',
+    dataType: 'json',
+    data: json,
+    contentType: 'application/json',
+    success: function(data) {
+      alert(data)
+    }
+  });
+});
+
+</script>
+
 </body>
 </html>
